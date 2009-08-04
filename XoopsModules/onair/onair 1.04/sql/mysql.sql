b41fc1752b15e37fef31d0a67570e4d28333e7cb
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-08-04 10:44:15 +0000

    added chart to use later

--- XoopsModules/onair/onair 1.04/sql/mysql.sql
+++ XoopsModules/onair/onair 1.04/sql/mysql.sql
@@ -30,10 +30,21 @@ PRIMARY KEY (pl_id)
 
 CREATE TABLE oa_hitlist (
   oa_songid int(10) unsigned NOT NULL auto_increment,
-  oa_songsong varchar(150) NOT NULL default '',
+  oa_songsong varchar(50) NOT NULL default '',
   oa_songtime varchar(100) NOT NULL default '',
   oa_songday varchar(1) NOT NULL,
   oa_songweek varchar(2) NOT NULL default '',
   oa_songyear varchar(4) NOT NULL default '',
   PRIMARY KEY  (oa_songid)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+
+CREATE TABLE oa_charts (
+  ch_songid int(10) unsigned NOT NULL auto_increment,
+  ch_songsong varchar(200) NOT NULL default '',
+  ch_songweekstotal varchar(3) NOT NULL default '',
+  ch_songlastweek varchar(3) NOT NULL,
+  ch_songtopplace varchar(3) NOT NULL default '',
+  ch_songthisweek varchar(3) NOT NULL default'',
+  ch_songweek varchar(2) NOT NULL default '',
+  PRIMARY KEY  (ch_songid)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
