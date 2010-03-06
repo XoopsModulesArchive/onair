851c9be943334a64a97640f92185e0484ce4ad1a
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-03-06 23:08:47 +0000

    Added another query to make block show up-comming event correctly

--- XoopsModules/onair/onair 1.04/blocks/onair_ajaxcall.php
+++ XoopsModules/onair/onair 1.04/blocks/onair_ajaxcall.php
@@ -102,10 +102,10 @@ function b_Onair_ajaxcall() {
 	$sqlnext2 = "SELECT * FROM  ".$xoopsDB->prefix("oa_program")." WHERE '$nextstop' >= oa_start AND '$nowday' = oa_day order by oa_start, oa_stop LIMIT 1";
 	$resultnext2=$xoopsDB->getRowsNum($sqlnext2);
 		if ($resultnext2 < 1 && $nowday == 6) {
-			$sqlnext = "SELECT * FROM  ".$xoopsDB->prefix("oa_program")." WHERE '$nextstop' <= oa_start AND '$nowday' = oa_day order by oa_start, oa_stop LIMIT 1";
+		 $sqlnext = "SELECT * FROM  ".$xoopsDB->prefix("oa_program")." WHERE '$nextstop' <= oa_start AND '$nowday' = oa_day order by oa_start, oa_stop LIMIT 1";
 			$resultnext=$xoopsDB->queryF($sqlnext);
 			if ($resultnext < 1) {
-			$sqlnext = "SELECT * FROM  ".$xoopsDB->prefix("oa_program")." WHERE '$nextbeginabsolute' <= oa_start AND '$nowday'+1 = oa_day order by oa_start, oa_stop LIMIT 1";
+			$sqlnext = "SELECT * FROM  ".$xoopsDB->prefix("oa_program")." WHERE '$nextbeginabsolute' <= oa_start AND '$nowday' = '0' order by oa_start, oa_stop LIMIT 1";
 			$resultnext=$xoopsDB->queryF($sqlnext);
 			}
 		}
