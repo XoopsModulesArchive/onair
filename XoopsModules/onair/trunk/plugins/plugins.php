fb9c157c216add2674fde127ccee21ca3ff045d3
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-06-30 16:12:32 +0000

    Removed the function onair_CheckSong() from plugin as this is no longer being used

--- XoopsModules/onair/trunk/plugins/plugins.php
+++ XoopsModules/onair/trunk/plugins/plugins.php
@@ -23,21 +23,18 @@ if ($oa_pluginname=='1') {
 // DireTTOre includefile informations
 $oa_pluginfile = onair_GetModuleOption('pluginfile_direttore');
 $oa_text2utf8 = onair_File_Get_Contents_Utf8($oa_pluginfile);
-onair_CheckSong($oa_text2utf8);
 $message['pluginshow'] = "<b>"._MB_ONAIR_NOWPLAYING.'</b><br/><a href="'.$oa_streamurl.'" target="_blank">'.$oa_text2utf8.'</a>';
 }
 if ($oa_pluginname=='2') {
 // StationPlaylist .txt-file includefile informations
 $oa_pluginfile = onair_GetModuleOption('pluginfile_stationplaylist');
 $oa_text2utf8 = onair_File_Get_Contents_Utf8($oa_pluginfile);
-onair_CheckSong($oa_text2utf8);
 $message['pluginshow'] = "<b>"._MB_ONAIR_NOWPLAYING.'</b><br><a href="'.$oa_streamurl.'" target="_blank">'.$oa_text2utf8.'</a>';
 }
 if ($oa_pluginname=='3') {
 // Winamp Nowplaying 1.4 by Antti Nevala & Lauri Nevala (txt-file includefile informations)
 $oa_pluginfile = onair_GetModuleOption('pluginfile_nowplaying');
 $oa_text2utf8 = onair_File_Get_Contents_Utf8($oa_pluginfile);
-onair_CheckSong($oa_text2utf8);
 $message['pluginshow'] = "<b>"._MB_ONAIR_NOWPLAYING.'</b><br><a href="'.$oa_streamurl.'" target="_blank">'.$oa_text2utf8.'</a>';
 }
 if ($oa_pluginname=='0') {
